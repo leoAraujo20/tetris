6b5b4144f1680ecf78c9d7f8e111ee27d907d8d8
Leônidas Araújo <leonidasaraujomqm@gmail.com>
2026-03-17 12:31:58 -0300

    Refactor Game and Grid classes to implement Tetromino randomization and position locking

--- tetris/game.py
+++ tetris/game.py
@@ -1,6 +1,9 @@
+import random
+
 import pygame
 from grid import Grid
-from tetrominos import *  # noqa: F403
+from tetromino import Tetromino
+from tetrominos import *
 
 
 class Game:
@@ -8,7 +11,15 @@ class Game:
         pygame.init()
         self.fps = 60
         self.screen = screen
-        self.current_tetro = ZTetromino()  # noqa: F405
+        self.tetros = [
+            ITetromino(),
+            STetromino(),
+            OTetromino(),
+            LTetromino(),
+            ZTetromino(),
+        ]
+        self.current_tetro = self._get_random_tetro()
+        self.next_tetro = self._get_random_tetro()
         self.clock = pygame.time.Clock()
         self.running = True
         self.game_grid = game_grid
@@ -30,6 +41,8 @@ class Game:
         self.current_tetro.move(1, 0)
         if not self._is_inside():
             self.current_tetro.move(-1, 0)
+            self._block_positions()
+            self.current_tetro = self._get_random_tetro()
 
     def rotate(self):
         self.current_tetro.rotate()
@@ -41,9 +54,29 @@ class Game:
         if not self._is_inside():
             self.current_tetro.rotate()
 
+    def _lock_positions(self):
+        positions = self.current_tetro.get_positions()
+        for position in positions:
+            self.game_grid.grid[position.y][position.x] = self.current_tetro.id
+        self.current_tetro = self.next_tetro
+        self.next_tetro = self._get_random_tetro()
+
     def _is_inside(self):
         positions = self.current_tetro.get_positions()
         for position in positions:
             if not self.game_grid.is_inside(position.y, position.x):
                 return False
         return True
+
+    def _get_random_tetro(self) -> Tetromino:
+        if len(self.tetros) == 0:
+            self.tetros = [
+                ITetromino(),
+                STetromino(),
+                OTetromino(),
+                LTetromino(),
+                ZTetromino(),
+            ]
+        tetro = random.choice(self.tetros)
+        self.tetros.remove(tetro)
+        return tetro
